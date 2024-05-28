eec1dfb96185a84067d66edb19e98e21e84d7d3e
jii1n <yj5596901@gmail.com>
2024-05-28 10:55:00 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from glob import glob
+from os.path import basename, splitext
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+install_requires = []
+
+
+setup(
+    name='mycalendar',  
+    version='1.0.0', 
+    py_modules=[splitext(basename(path))[0] for path in glob('*.py')],  
+    install_requires=install_requires,
+    author='jii1n',
+    author_email='yj5596901@gmail.com',
+    description='Desc'
+)
